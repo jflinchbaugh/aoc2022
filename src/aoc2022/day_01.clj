ea65b76ad0b73b2d0440aa9989596bf67f4cadba
John Flinchbaugh <john@hjsoft.com>
2022-12-01 00:18:55 -0500

    day 1, part 1 & 2

--- src/aoc2022/day_01.clj
+++ src/aoc2022/day_01.clj
@@ -1,2 +1,38 @@
 (ns aoc2022.day-01
-  (:require [aoc2022.core :refer :all]))
+  (:require [aoc2022.core :refer :all]
+            [clojure.string :as str]))
+
+(defn part-1 []
+  (->>
+    "src/aoc2022/day_01.txt"
+    file->lines
+    (partition-by str/blank?)
+    (remove #{'("")})
+    (map
+      (fn [cals] (reduce + (map parse-long cals))))
+    (apply max)))
+
+(defn part-2 []
+  (->>
+    "src/aoc2022/day_01.txt"
+    file->lines
+    (partition-by str/blank?)
+    (remove #{'("")})
+    (map
+      (fn [cals] (reduce + (map parse-long cals))))
+    sort
+    reverse
+    (take 3)
+    (reduce +))
+  )
+
+(comment
+
+  (part-1)
+  ;; => 70374
+
+
+  (part-2)
+  ;; => 204610
+
+  )

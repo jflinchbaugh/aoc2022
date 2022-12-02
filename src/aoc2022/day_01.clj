7dbb86db618e1ed6a2be77a996f325fb0add9e50
John Flinchbaugh <john@hjsoft.com>
2022-12-01 21:29:06 -0500

    refactor

--- src/aoc2022/day_01.clj
+++ src/aoc2022/day_01.clj
@@ -2,37 +2,34 @@
   (:require [aoc2022.core :refer :all]
             [clojure.string :as str]))
 
+(defn lines->total-calories [lines]
+  (->> lines
+       (partition-by str/blank?)
+       (remove #{'("")})
+       (map
+         (fn [cals] (reduce + (map parse-long cals))))
+       (sort >)))
+
 (defn part-1 []
   (->>
-    "src/aoc2022/day_01.txt"
-    file->lines
-    (partition-by str/blank?)
-    (remove #{'("")})
-    (map
-      (fn [cals] (reduce + (map parse-long cals))))
-    (apply max)))
+   "src/aoc2022/day_01.txt"
+   file->lines
+   lines->total-calories
+   first))
 
 (defn part-2 []
   (->>
-    "src/aoc2022/day_01.txt"
-    file->lines
-    (partition-by str/blank?)
-    (remove #{'("")})
-    (map
-      (fn [cals] (reduce + (map parse-long cals))))
-    sort
-    reverse
-    (take 3)
-    (reduce +))
-  )
+   "src/aoc2022/day_01.txt"
+   file->lines
+   lines->total-calories
+   (take 3)
+   (reduce +)))
 
 (comment
 
   (part-1)
   ;; => 70374
 
-
   (part-2)
   ;; => 204610
-
   )

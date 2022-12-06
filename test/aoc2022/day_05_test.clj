6a9856b5343aecd3148d37a2c54bb7e3a7a26549
John Flinchbaugh <john@hjsoft.com>
2022-12-06 07:57:03 -0500

    remove old test for code that's gone

--- test/aoc2022/day_05_test.clj
+++ test/aoc2022/day_05_test.clj
@@ -2,15 +2,6 @@
   (:require [aoc2022.day-05 :as sut]
             [clojure.test :as t]))
 
-(t/deftest test-move-stacks
-  (t/is (= [[\a \b \c]
-            [\d \e]
-            []] (sut/move-stacks [[\a \b \c] [\d] [\e]] [1 3 2])))
-
-  (t/is (= [[\a]
-            [\d \c \b]
-            [\e]] (sut/move-stacks [[\a \b \c] [\d] [\e]] [2 1 2]))))
-
 (t/deftest test-parse-moves
   (t/is (= [[10 9 8]
             [3 9 4]

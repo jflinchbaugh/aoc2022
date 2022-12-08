14ba80217e46906827ea1a9623297348465b9035
John Flinchbaugh <john@hjsoft.com>
2022-12-08 18:40:20 -0500

    day 8 part 2

--- src/aoc2022/day_08.clj
+++ src/aoc2022/day_08.clj
@@ -14,32 +14,59 @@
 
 (defn part-1 []
   (let [nswe (->>
-                "src/aoc2022/day_08.txt"
-                file->lines
-                lines->matrix)
+              "src/aoc2022/day_08.txt"
+              file->lines
+              lines->matrix)
         wens (apply mapv vector nswe)
         height (count nswe)
         width (count wens)]
     (->>
-      (for [x (range width)
-            y (range height)]
-        (let [height (get-in wens [x y])
-              [view-west [_ & view-east]] (split-at x (get nswe y))
-              [view-north [_ & view-south]] (split-at y (get wens x))]
-          (assert (get-in nswe [y x]) (get-in wens [x y]))
-          (or
-            (seen? height view-east)
-            (seen? height view-west)
-            (seen? height view-north)
-            (seen? height view-south))))
-      (filter identity)
-      count)))
+     (for [x (range width)
+           y (range height)]
+       (let [height (get-in wens [x y])
+             [view-west [_ & view-east]] (split-at x (get nswe y))
+             [view-north [_ & view-south]] (split-at y (get wens x))]
+         (or
+          (seen? height view-east)
+          (seen? height (reverse view-west))
+          (seen? height (reverse view-north))
+          (seen? height view-south))))
+     (filter identity)
+     count)))
 
+(defn visibility [height view]
+  (let [too-far (count (take-while (partial >= height) view))
+        too-short (count (take-while (partial > height) view))]
+    (if (= too-far too-short) too-short (inc too-short)))
+  )
+
+(defn part-2 []
+  (let [nswe (->>
+              "src/aoc2022/day_08.txt"
+              file->lines
+              lines->matrix)
+        wens (apply mapv vector nswe)
+        height (count nswe)
+        width (count wens)]
+    (->>
+     (for [x (range width)
+           y (range height)]
+       (let [height (get-in wens [x y])
+             [view-west [_ & view-east]] (split-at x (get nswe y))
+             [view-north [_ & view-south]] (split-at y (get wens x))]
+         (*
+          (visibility height view-east)
+          (visibility height (reverse view-west))
+          (visibility height (reverse view-north))
+          (visibility height view-south))))
+     (sort >)
+     first)))
 
 (comment
   (part-1)
   ;; => 1717
 
-
+  (part-2)
+  ;; => 321975
 
   nil)
--- test/aoc2022/day_08_test.clj
+++ test/aoc2022/day_08_test.clj
@@ -2,5 +2,16 @@
   (:require [aoc2022.day-08 :as sut]
             [clojure.test :as t]))
 
+(t/deftest test-visibility
+  (t/is (= 0 (sut/visibility 5 [])))
+  (t/is (= 1 (sut/visibility 5 [4])))
+  (t/is (= 1 (sut/visibility 5 [5])))
+  (t/is (= 1 (sut/visibility 5 [5 5])))
+  (t/is (= 1 (sut/visibility 5 [5 4])))
+  (t/is (= 2 (sut/visibility 5 [4 5 4])))
+ )
+
 (t/deftest test-answers
-  (t/is (= 1717 (sut/part-1))))
+  (t/is (= 1717 (sut/part-1)))
+  (t/is (= 321975 (sut/part-2))))
+

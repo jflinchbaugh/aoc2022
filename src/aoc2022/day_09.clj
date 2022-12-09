2e4947c2fb85593dbb596457a532bbe933c40ec5
John Flinchbaugh <john@hjsoft.com>
2022-12-09 08:20:51 -0500

    day 9 part 1

---
+++ src/aoc2022/day_09.clj
@@ -0,0 +1,54 @@
+(ns aoc2022.day-09
+  (:require [aoc2022.core :refer :all]
+            [clojure.string :as str]))
+
+(defn parse-line [line]
+  (let [[dir dist] (str/split line #" ")]
+    [({"U" :up "D" :down "L" :left "R" :right} dir)
+     (parse-long dist)]))
+
+(defn expand [[dir dist]]
+  (repeat dist dir))
+
+(defn move-1 [[[cx cy] & path] dir]
+  (cons
+    (case dir
+      :up [cx (dec cy)]
+      :down [cx (inc cy)]
+      :left [(dec cx) cy]
+      :right [(inc cx) cy]
+      [cx cy])
+    (cons [cx cy] path)))
+
+(defn follow-1 [[[tx ty] & tail-path] [hx hy]]
+  (cons
+    (let [dx (- hx tx)
+          dy (- hy ty)]
+      (if (and (>= 1 (abs dx)) (>= 1 (abs dy)))
+        [tx ty]
+
+        [(cond (pos? dx) (inc tx) (neg? dx) (dec tx) :else tx)
+         (cond (pos? dy) (inc ty) (neg? dy) (dec ty) :else ty)]
+        )
+      )
+    (cons [tx ty] tail-path)))
+
+(defn part-1 []
+  (->>
+    "src/aoc2022/day_09.txt"
+    file->lines
+    (map parse-line)
+    (mapcat expand)
+    (reduce move-1 [[0 0]])
+    reverse
+    (reduce follow-1 [[0 0]])
+    set
+    count))
+
+(comment
+
+  (part-1)
+  ;; => 6464
+
+
+  nil)
---
+++ test/aoc2022/day_09_test.clj
@@ -0,0 +1,64 @@
+(ns aoc2022.day-09-test
+  (:require [aoc2022.day-09 :as sut]
+            [clojure.test :as t]))
+
+(t/deftest test-move-1
+  (t/is (= [[0 -1] [0 0]] (sut/move-1 [[0 0]] :up)))
+  (t/is (= [[0 1] [0 0]] (sut/move-1 [[0 0]] :down)))
+  (t/is (= [[-1 0] [0 0]] (sut/move-1 [[0 0]] :left)))
+  (t/is (= [[1 0] [0 0]] (sut/move-1 [[0 0]] :right)))
+
+  (t/is (= [[1 0] [0 0] [10 10]] (sut/move-1 [[0 0] [10 10]] :right)))
+
+  (t/is (= [[0 0] [0 0] [10 10]] (sut/move-1 [[0 0] [10 10]] :invalid)))
+  )
+
+(t/deftest test-follow-1
+  (t/testing "adjacent positions don't move the tail"
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [0 0])))
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [0 -1])))
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [1 -1])))
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [1 0])))
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [1 1])))
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [0 1])))
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [-1 1])))
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [-1 0])))
+    (t/is (= [[0 0] [0 0]] (sut/follow-1 [[0 0]] [-1 -1]))))
+
+  (t/testing "further positions move tail"
+    (t/is (= [[0 1] [0 0]] (sut/follow-1 [[0 0]] [0 2])))
+    (t/is (= [[0 -1] [0 0]] (sut/follow-1 [[0 0]] [0 -2])))
+    (t/is (= [[1 0] [0 0]] (sut/follow-1 [[0 0]] [2 0])))
+    (t/is (= [[-1 0] [0 0]] (sut/follow-1 [[0 0]] [-2 0])))
+
+    (t/is (= [[1 1] [0 0]] (sut/follow-1 [[0 0]] [2 2])))
+    (t/is (= [[1 1] [0 0]] (sut/follow-1 [[0 0]] [2 1])))
+    (t/is (= [[1 1] [0 0]] (sut/follow-1 [[0 0]] [1 2])))
+
+    (t/is (= [[-1 1] [0 0]] (sut/follow-1 [[0 0]] [-2 2])))
+    (t/is (= [[-1 1] [0 0]] (sut/follow-1 [[0 0]] [-2 1])))
+    (t/is (= [[-1 1] [0 0]] (sut/follow-1 [[0 0]] [-1 2])))
+
+    (t/is (= [[-1 -1] [0 0]] (sut/follow-1 [[0 0]] [-2 -2])))
+    (t/is (= [[-1 -1] [0 0]] (sut/follow-1 [[0 0]] [-2 -1])))
+    (t/is (= [[-1 -1] [0 0]] (sut/follow-1 [[0 0]] [-1 -2])))
+
+    (t/is (= [[1 -1] [0 0]] (sut/follow-1 [[0 0]] [2 -2])))
+    (t/is (= [[1 -1] [0 0]] (sut/follow-1 [[0 0]] [2 -1])))
+    (t/is (= [[1 -1] [0 0]] (sut/follow-1 [[0 0]] [1 -2]))))
+
+  (t/testing "rest of the tail is left intact"
+    (t/is (= [[-1 -1] [0 0] [10 10]] (sut/follow-1 [[0 0] [10 10]] [-2 -2])))))
+
+(t/deftest test-expand
+  (t/is (= [:up :up] (sut/expand [:up 2]))))
+
+(t/deftest test-parse-line
+  (t/is (= [:up 2] (sut/parse-line "U 2")))
+  (t/is (= [:down 2] (sut/parse-line "D 2")))
+  (t/is (= [:right 2] (sut/parse-line "R 2")))
+  (t/is (= [:left 2] (sut/parse-line "L 2")))
+  )
+
+(t/deftest sample-head
+  (t/is (= 6464 (sut/part-1))))

f28d25e39faa4047c1ea52010568f27193507686
John Flinchbaugh <john@hjsoft.com>
2022-12-15 00:15:21 -0500

    day 12 part 2

--- src/aoc2022/day_12.clj
+++ src/aoc2022/day_12.clj
@@ -22,7 +22,7 @@
         (fn [m k v] (assoc m k (map second v)))
         {})))
 
-(defn find-next [nodes [row col val]]
+(defn find-next-up [nodes [row col val]]
   (filter
    (fn [[sr sc sv]]
      (and
@@ -32,7 +32,7 @@
          [(inc row) col]
          [(dec row) col]} [sr sc]))) nodes))
 
-(defn build-graph [nodes]
+(defn build-graph-up [nodes]
   (into {}
     (map
       (fn [node]
@@ -40,7 +40,7 @@
          (into {}
            (map
              (fn [n c] [n c])
-             (find-next nodes node)
+             (find-next-up nodes node)
              (repeat 1)))])
       nodes)))
 
@@ -60,15 +60,56 @@
         number-grid (map
                      (fn [[r c letter]] [r c (letter->val letter)])
                      source-grid)
-        graph (build-graph number-grid)]
-    (first (vals (dijkstra graph start end)))
-    )
+        graph (build-graph-up number-grid)]
+    (first (vals (dijkstra graph start end))))
   )
+
+(defn find-next-down [nodes [row col val]]
+  (filter
+    (fn [[sr sc sv]]
+      (and
+        (>= sv (dec val))
+        (#{[row (inc col)]
+           [row (dec col)]
+           [(inc row) col]
+           [(dec row) col]} [sr sc]))) nodes))
+
+(defn build-graph-down [nodes]
+  (into {}
+    (map
+      (fn [node]
+        [node
+         (into {}
+           (map
+             (fn [n c] [n c])
+             (find-next-down nodes node)
+             (repeat 1)))])
+      nodes)))
+
+(defn part-2 []
+  (let [source-grid (->> "src/aoc2022/day_12.txt"
+                      file->lines
+                      lines->grid)
+        start (terminal source-grid \S 0)
+        end (terminal source-grid \E 25)
+        number-grid (map
+                      (fn [[r c letter]] [r c (letter->val letter)])
+                      source-grid)
+        graph (build-graph-down number-grid)]
+    (->>
+      (dijkstra graph end)
+      (filter (fn [[[r c e] cost]] (zero? e)))
+      (sort-by second)
+      first
+      second)))
 
 (comment
 
   (part-1)
   ;; => 484
 
+
+  (part-2)
+  ;; => 478
 
   nil)

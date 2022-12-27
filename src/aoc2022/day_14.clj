ed0b7ad5783fa85a93f9a89d3999530b38a6a83f
John Flinchbaugh <john@hjsoft.com>
2022-12-26 23:56:21 -0500

    day 14 part 2

--- src/aoc2022/day_14.clj
+++ src/aoc2022/day_14.clj
@@ -22,22 +22,25 @@
    (partition 2 1)
    (mapcat (partial apply expand))))
 
-(defn drop-1 [[[x y] stones field]]
+(defn drop-1 [[[x y] stones field floor]]
   (cond
     (nil? x)
     (throw (Exception. "no stone to drop"))
 
+    (and floor (>= y (dec floor)))
+    [[500 0] (conj stones [x y]) field floor]
+
     (not (or (stones [x (inc y)]) (field [x (inc y)])))
-    [[x (inc y)] stones field]
+    [[x (inc y)] stones field floor]
 
     (not (or (stones [(dec x) (inc y)]) (field [(dec x) (inc y)])))
-    [[(dec x) (inc y)] stones field]
+    [[(dec x) (inc y)] stones field floor]
 
     (not (or (stones [(inc x) (inc y)]) (field [(inc x) (inc y)])))
-    [[(inc x) (inc y)] stones field]
+    [[(inc x) (inc y)] stones field floor]
 
     :else
-    [[500 0] (conj stones [x y]) field]))
+    [[500 0] (conj stones [x y]) field floor]))
 
 (defn overflow? [lowest-y [[_ y]]]
   (> y lowest-y))
@@ -58,10 +61,34 @@
       count
       )))
 
+(defn full? [entry-point [_ stones]]
+  (stones entry-point))
+
+(defn part-2 []
+  (let [field (->>
+                "src/aoc2022/day_14.txt"
+                file->lines
+                (map parse)
+                (mapcat expand-line)
+                set)
+        floor (+ 2 (apply max (map second field)))]
+    (->>
+      (iterate drop-1 [[500 0] #{} field floor])
+      (take-while (complement (partial full? [500 0])))
+      last
+      second
+      count
+      inc ;; since we stopped and discarded the case for [500 0]
+      )))
+
 (comment
 
   (part-1)
   ;; => 737
+
+  (part-2)
+  ;; => 28145
+
 
   nil)
 
